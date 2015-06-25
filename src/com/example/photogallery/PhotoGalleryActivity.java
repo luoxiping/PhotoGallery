7f6fade9736f998a8240d97323066997d4b4235f
luoxp <luoxp@jugame.cn>
2015-06-25 16:58:47 +0800

    First commit

---
+++ src/com/example/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,17 @@
+package com.example.photogallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		return new PhotoGalleryFragment();
+	}
+
+}
---
+++ src/com/example/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,47 @@
+package com.example.photogallery;
+
+import java.io.IOException;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class PhotoGalleryFragment extends Fragment {
+	private static final String TAG = "PhotoGalleryFragment";
+	private GridView mGridView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+		new FetchItemsTask().execute();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+		mGridView = (GridView) v.findViewById(R.id.gridView);
+		return v;
+	}
+	
+	private class FetchItemsTask extends AsyncTask<Void, Void, Void>{
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				String result = new FlickrFetchr().getUrl("https://www.baidu.com/");
+				Log.i(TAG, "Result:" + result);
+			} catch (IOException e) {
+				Log.e(TAG, "Fail:" + e);
+			}
+			return null;
+		}
+		
+	}
+	
+}
