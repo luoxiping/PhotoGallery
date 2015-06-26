351eb8a63b5ddf41b875e2e6e9b0f6029918e057
luoxp <luoxp@jugame.cn>
2015-06-26 10:19:18 +0800

    fix

--- src/com/example/photogallery/PhotoGalleryActivity.java
+++ src/com/example/photogallery/PhotoGalleryActivity.java
@@ -1,11 +1,6 @@
 package com.example.photogallery;
 
-import android.app.Activity;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
 
